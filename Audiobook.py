16b13ab038f345b9ec3a61f22b215e0c67d88b22
Danita Velasquez <danitavelasquez7002@gmail.com>
2026-02-27 13:46:42 +0800

    First Commit

---
+++ Audiobook.py
@@ -0,0 +1,19 @@
+import pyttsx3
+from PyPDF2 import PdfReader
+
+# Open PDF
+book = open('orv.pdf', 'rb')
+pdfReader = PdfReader(book)
+
+# Pages
+pages = len(pdfReader.pages)
+print("Total pages:", pages)
+
+# Page
+page = pdfReader.pages[18]
+text = page.extract_text()
+
+# Speak
+speaker = pyttsx3.init()
+speaker.say(text)
+speaker.runAndWait()
